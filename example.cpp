0a0426c4adb5d3a94e36d86eeba737130f5658ee
IAmSaad <saadmahmoodmughal@gmail.com>
2016-04-08 23:40:47 +0500

    Added files via upload
    an example file showing the usage of stack

---
+++ example.cpp
@@ -0,0 +1,27 @@
+#include"Stack.h"
+#include<iostream>
+using namespace std;
+
+int main() {
+	stack pileOfBooks;
+	
+	pileOfBooks.push(100);
+	pileOfBooks.push(200);
+	pileOfBooks.push(300);
+	pileOfBooks.push(400);
+	pileOfBooks.push(500);
+	
+	pileOfBooks.displayFull();
+
+	cout<<pileOfBooks.peek()<<endl;
+
+	pileOfBooks.pop();
+
+	pileOfBooks.displayFull();
+
+	cout << pileOfBooks.peek() << endl;
+
+
+	system("pause");
+	return 0;
+}
